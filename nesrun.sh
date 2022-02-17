da10879a5b4f4ea4656fb31dfb0ac728a30aca9f
swankidelic <swankidelic@gmail.com>
2022-02-17 16:24:05 -0600

    refactored to use includes and config file

--- nesrun.sh
+++ nesrun.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/bash
-echo "compiling .o file..."
-ca65 $1".asm"
-echo ".o file ok"
-echo "compiling .nes file..."
-ld65 $1".o" -t nes -o $1".nes"
+echo "compiling "$1
+ca65 "src/"$1".asm"
+echo "compiling reset"
+ca65 src/reset.asm
+echo "compiling assembly ok"
+echo "linking .nes file..."
+ld65 src/reset.o "src/"$1".o" -C nes.cfg -o $1".nes"
 echo ".nes file ok"
 echo "running emulator..."
 /c/cc65/fceux/fceux.exe $1".nes"
